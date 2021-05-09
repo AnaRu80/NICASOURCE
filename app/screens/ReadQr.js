15993df41e76aa6b6e0cda3a6c3529c313153892
ana.ra <ana.ra@eucledian.com>
2021-05-09 13:07:18 -0500

    Read QR Screen
     Scanner screen with alert to know if you want to save the link or not, still not having the functionality to pass to the list

--- app/screens/ReadQr.js
+++ app/screens/ReadQr.js
@@ -1,13 +1,44 @@
-import React from "react";
-import Constants from "expo-constants";
-import { StyleSheet, Text } from "react-native";
+import React, { useState, useEffect } from "react";
+import { Text, View, StyleSheet, Button, Alert } from "react-native";
+import { BarCodeScanner } from "expo-barcode-scanner";
 
 import Screen from "../components/Screen";
 
 function ReadQr() {
+  // Permission for camera access
+  const requestPermission = async () => {
+    const { granted } = await BarCodeScanner.requestPermissionsAsync();
+    if (!granted) alert("You need to enable your permissions");
+  };
+
+  const [scanned, setScanned] = useState(false);
+
+  useEffect(() => {
+    requestPermission();
+  }, []);
+
+  // Handle Scan without functionality
+  const handleBarCodeScanned = ({ type, data }) => {
+    setScanned(true);
+    Alert.alert(
+      "Scan",
+      `Bar code with data ${data} has been scanned!Save Link?`,
+      [
+        { text: "Yes", onPress: () => console.log("scanner") },
+        { text: "No", onPress: () => setScanned(false) },
+      ]
+    );
+  };
+
   return (
-    <Screen style={styles.screen}>
-      <Text style={styles.text}>QR Read</Text>
+    <Screen>
+      <Text style={styles.text}>QR READ</Text>
+      <View style={styles.container}>
+        <BarCodeScanner
+          onBarCodeScanned={scanned ? undefined : handleBarCodeScanned}
+          style={StyleSheet.absoluteFillObject}
+        />
+      </View>
     </Screen>
   );
 }
@@ -15,9 +46,18 @@ function ReadQr() {
 export default ReadQr;
 
 const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
   screen: {
     flex: 1,
     backgroundColor: "#f9eeec",
     alignItems: "center",
+  },
+  text: {
+    fontSize: 30,
+    fontWeight: "bold",
+    alignSelf: "center",
+    marginTop: 10,
   },
 });
